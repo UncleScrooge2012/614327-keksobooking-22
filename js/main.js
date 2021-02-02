b9a208572c09612785d65fcf6492f3910e206db0
UncleScrooge2012 <naruk2024@gmail.com>
2021-02-02 15:51:09 +0300

    fix cod
    fix error

--- js/main.js
+++ js/main.js
@@ -1,32 +1,29 @@
 
 let numberRandomRange = function (min, max) {
-if (min >= 0 && max >= 0) {
-  return Math.floor(Math.random() * (max - min + 1)) + min  //Максимум и минимум включаются
+  if (min >= 0 && max >= 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min  //Максимум и минимум включаются
+  }
 
-
-
-}
-
-alert(`Занчение min = ${min} и max = ${max} должны быть положительные, больще 0 `)
+  alert(`Занчение min = ${min} и max = ${max} должны быть положительные, больще 0 `)
 
 };
 
 
 let numberRandomRange2 = function (min, max, comma = 0) {
   if (min >= 0 && max >= 0 && comma >= 0) {
-    let number = Math.random() * (max - min + 1) + min
     let result = +number.toFixed(comma)
-
+    let number = Math.random() * (max - min + 1) + min
     return result
 
   }
 
-  alert(`Занчение min = ${min} , max = ${max} comma = ${comma} должны быть положительные, больще 0 `)
+  alert(`Занчение min = ${min} , max = ${max} comma = ${comma} должны быть положительные, больще 0 `);
 
-  };
+};
 
+numberRandomRange(2, 5);
 
-
+numberRandomRange2(1, 6 , 3);
 
 
 
