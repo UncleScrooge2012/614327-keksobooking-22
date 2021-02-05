4b747a1919b64271465d4917c416222f1d279bc5
UncleScrooge2012 <naruk2024@gmail.com>
2021-02-05 20:09:38 +0300

    add generaterAds
    did 3 task

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ const generateNumberRange = function (min, max) {
 
 };
 
-
 const generateRandomRangeWithComma = function (min, max, comma = 0) {
   if (min >= 0 && max >= 0 && comma >= 0) {
     if (min === max ) {
@@ -29,7 +28,127 @@ generateNumberRange(2, 5);
 
 alert(generateRandomRangeWithComma(1, 1 , 3))
 
+const TITLE = ['Отели', 'Апартаменты/квартиры', 'Курортные отели', 'Виллы', 'Шале', 'Коттеджи', 'Глэмпинг']; // заголовок предложения
+const PRICE = [20000, 40000, 50000, 70000, 150000, 32000, 90000]; // Рандом цен
+const TYPE = ['palace', 'flat', 'house', 'bungalow']; // одно из четырёх фиксированных значений
+const TIME = ['12:00', '13:00', '14:00']; //  одно из трёх фиксированных значений
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; // преимущества
+const DESCRIPTION = ['панорамным видом на Средиземное море оснащены',
+  'кондиционером', 'телевизором с плоским экраном и международными каналами',
+  'В каждом номере можно воспользоваться принадлежностями для чая/кофе.',
+  'В распоряжении гостей собственная ванная комната с ванной',
+  'душевой кабиной и роскошными туалетно-косметическими принадлежностями',
+  'С балкона или террасы всех номеров открывается панорамный вид на Средиземное море.',
+];
+
+const generateNumber = function (min = 1, max = 8) {     // Функция для рандомного чилса range
+  return  Math.floor( Math.random() * (max - min + 1) + min )
+};
+
+const generate = function (array) {  // Функция для рандомного выбора
+  return array[Math.floor(Math.random() * array.length)]
+};
+
+const generateRandom = function (array) {  // функция для  рандомного  создания массива
+  const randomNumber = Math.floor(Math.random() * array.length + 1  );
+  const newArray = [];
+  for(let i = 0; i < randomNumber ; i++ ) {
+    const randomArray = array.filter((currentValue) => {
+      return newArray.indexOf(currentValue) === -1
+    })
+    newArray.push(randomArray[Math.floor(Math.random() * randomArray.length )])
+  }
+  return newArray
+};
+
+const genraterFoto = function () { // Функция для закгрузки фотографии
+  const randomNumber = generateNumber();
+  const photosArray = [];
+  for(let i = 1; i <= randomNumber; i++  ){
+    photosArray.push(`http://o0.github.io/assets/images/tokyo/hotel${i}.jpg`)
+  }
+  return photosArray
+};
+
+const genraterLocation = function () {  // Функция по оси X и Y
+
+  const genraterLocationWhisX = function (min = 35.65000, max = 35.70000, comma = 5) {  // Фукция по оси X
+
+    if (min >= 0 && max >= 0 && comma >= 0) {
+      if (min === max ) {
+        return min
+
+      }
+      const number = Math.random() * (max - min + 1) + min
+      return  +number.toFixed(comma)
+
+    }
+  };
+
+  const genraterLocationWhisY = function (min = 139.70000, max = 139.80000, comma = 5) { // Функция по оси Y
+    if (min >= 0 && max >= 0 && comma >= 0) {
+      if (min === max ) {
+        return min
+      }
+      const number = Math.random() * (max - min + 1) + min
+      return  +number.toFixed(comma)
+    }
+  };
+
+  const location = {};
+
+  location.x = genraterLocationWhisX();
+  location.y = genraterLocationWhisY();
+  return location
+}
+
+const generateOffer = function () {  // функция генерирует случайную информацию
+
+  const offer = {};
+  offer.title = generate(TITLE);
+  offer.address = genraterLocation();
+  offer.price = generate(PRICE);
+  offer.type = generate(TYPE);
+  offer.rooms = generateNumber();
+  offer.guests = generateNumber();
+  offer.checkin = generate(TIME);
+  offer.checkout = generate(TIME);
+  offer.features = generateRandom(FEATURES);
+  offer.description = generateRandom(DESCRIPTION);
+  offer.photos = genraterFoto();
+
+
+  return offer
+}
+
+//написать функцию , которая создает масиив из 10 сгенерированных JS-объектов.
+
+
+// 1) объект авотар
+
+// 1.1) создать функуцию которая рандомно будет присваевать строке число от 1 до 8 с ведушим 0
+
+const generateAvatar = function ()  { // Функция случайное число с ведушим нулем
+  const number = '0';
+  const randomNumber = number + generateNumber();
+  const randomString = `img/avatars/user${randomNumber}.png`;
+  return randomString
+};
 
 
 
 
+const generateAds = function () { // функция создает объявления
+  const adsArray = [];
+  for (let i = 1; i < 4; i++ ) {
+    let ad = 'Ad' + i;
+    ad = new Object();
+    ad.author = generateAvatar();
+    ad.offer = generateOffer() ;
+    ad.location = ad.offer.address;
+    adsArray.push(ad)
+  }
+  return adsArray
+};
+
+generateAds()
